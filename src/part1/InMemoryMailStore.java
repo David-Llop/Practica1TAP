73543199a7071b39a8a72a8730ed72727d830c6d
David-Llop <58088372+David-Llop@users.noreply.github.com>
2020-10-15 19:23:36 +0200

    Interficie MailStore i javadoc

--- src/part1/InMemoryMailStore.java
+++ src/part1/InMemoryMailStore.java
@@ -1,4 +1,13 @@
 package part1;
 
 public class InMemoryMailStore implements MailStore{
+    @Override
+    public void sendMail(Message mail) {
+
+    }
+
+    @Override
+    public Message[] getMail(User user) {
+        return new Message[0];
+    }
 }
--- src/part1/MailStore.java
+++ src/part1/MailStore.java
@@ -1,4 +1,17 @@
 package part1;
 
 public interface MailStore {
+
+    /**
+     * Function that sends a given Message
+     * @param mail {@link Message} to be send
+     */
+    void sendMail(Message mail);
+
+    /**
+     * Function that, given a {@link User}, returns the {@link Message} [] intended for it
+     * @param user {@link User} for whom the mails are intended
+     * @return {@link Message} [] of mails intended for the given user
+     */
+    Message[] getMail(User user);
 }
