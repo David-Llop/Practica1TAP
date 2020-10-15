454ba731113e8ab2c55f9b4079724b06692199cb
David-Llop <58088372+David-Llop@users.noreply.github.com>
2020-10-15 20:07:07 +0200

    Interficie MailStore i javadoc

--- src/part1/InMemoryMailStore.java
+++ src/part1/InMemoryMailStore.java
@@ -1,11 +1,20 @@
 package part1;
 
 public class InMemoryMailStore implements MailStore{
+    /**
+     * Function that sends a given Message
+     * @param mail {@link Message} to be send
+     */
     @Override
     public void sendMail(Message mail) {
 
     }
 
+    /**
+     *Function that, given a {@link User}, returns the {@link Message} [] intended for it
+     * @param user {@link User} for whom the mails are intended
+     * @return {@link Message} [] of mails intended for the given user
+     */
     @Override
     public Message[] getMail(User user) {
         return new Message[0];
