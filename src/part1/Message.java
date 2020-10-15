5dcd63adf1b5ce929d160d9b482be032031629b5
Anna <annajulia@estudiants.urv.cat>
2020-10-15 18:57:53 +0200

    "Coses basiques USER i MAIL"

--- src/part1/Message.java
+++ src/part1/Message.java
@@ -1,4 +1,60 @@
 package part1;
 
+import java.util.Date;
+
 public class Message {
+//   * part1.Message
+//   + de: part1.User envia (username)
+//   + per: part1.User rep (username)
+
+    private String text, title, from, to;
+    private Date maildate; //Date(int year, int month, int date, int hrs, int min)
+
+    public Message(String text, String title, String from, String to, Date maildate) {
+        this.text = text;
+        this.title = title;
+        this.from = from;
+        this.to = to;
+        this.maildate = maildate;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public Date getMaildate() {
+        return maildate;
+    }
+
+    public void setMaildate(Date maildate) {
+        this.maildate = maildate;
+    }
 }
--- src/part1/User.java
+++ src/part1/User.java
@@ -3,7 +3,37 @@ package part1;
 import  java.lang.Object;
 import java.util.Date;
 
-public class User{
+public class User {
     private String username, name;
-    private Date birthdate;
+    private Date birthdate; //Date(int year, int month, int date)
+
+    public User(String username, String name, Date birthdate) {
+        this.username = username;
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
 }
