114ce063f4b5eb604e98417ab519bc2711b5732f
Anna <annajulia@estudiants.urv.cat>
2020-10-22 11:10:12 +0200

    "MAIL retocado"

--- src/part1/Message.java
+++ src/part1/Message.java
@@ -1,22 +1,42 @@
 package part1;
 
+import java.util.Date;
+
 public class Message {
-    public Message(String from, String to, String subject, String text) {
+    private String text, from, to, subject;
+    private Date date;
+    private int nWords;
+
+    public Message(String text, String from, String to, String subject, Date date, int nWords) {
+        this.text = text;
+        this.from = from;
+        this.to = to;
+        this.subject = subject;
+        this.date = date;
+        this.nWords = nWords;
     }
 
-    public String getFrom() {
-        return null;
-    }
+    public void setText(String text) { this.text = text; }
 
-    public String getTo() {
-        return null;
-    }
+    public void setFrom(String from) { this.from = from; }
 
-    public String getSubject() {
-        return null;
-    }
+    public void setTo(String to) {this.to = to; }
 
-    public String getText() {
-        return null;
-    }
+    public void setSubject(String subject) { this.subject = subject; }
+
+    public Date getDate() { return date; }
+
+    public void setDate(Date date) { this.date = date; }
+
+    public int getnWords() { return nWords; }
+
+    public void setnWords(int nWords) { this.nWords = nWords; }
+
+    public String getFrom() { return from; }
+
+    public String getTo() { return to; }
+
+    public String getSubject() { return subject; }
+
+    public String getText() { return text; }
 }
