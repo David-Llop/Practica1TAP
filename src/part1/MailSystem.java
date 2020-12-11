a55f40e10ee0cbed15f18dd66a33bef9c6f87156
David-Llop <58088372+David-Llop@users.noreply.github.com>
2020-12-11 11:41:20 +0100

    MailBox iterable

--- src/part1/MailSystem.java
+++ src/part1/MailSystem.java
@@ -1,5 +1,16 @@
 package part1;
 
+import java.util.ArrayList;
+
 public class MailSystem {
+    MailStore mailStore;
+    ArrayList<User> usersList;
+
+    public MailBox addUser(User user){
+        if (!usersList.contains(user)){
+            usersList.add(user);
+        }
+        return new MailBox(mailStore, user);
+    }
 
 }
