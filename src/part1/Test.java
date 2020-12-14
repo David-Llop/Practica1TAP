61105ad76097986270ad00fe6ab110e7079c74a8
Anna <annajulia@estudiants.urv.cat>
2020-12-14 10:44:55 +0100

    "Intento main pero soc inutil <3 amb la millor conver de la vida"

--- src/part1/Test.java
+++ src/part1/Test.java
@@ -17,6 +17,14 @@ public class Test
         MailStore mailStore = mailSystem.getMailStore();
         AnnaJu128.sendMail(num1);
         mailStore.getMail("Llop00");
-        Message num2 = new Message();
+        Message num2 = new Message("Llop00", "AnnaJu128", "Cafe", "Si perfecte! pero a partir de les 5. On vols quedar? al bar de la uni?");
+        Llop00.sendMail(num2);
+        mailStore.getMail("AnnaJu128");
+        Message num3 = new Message("AnnaJu128", "Llop00", "Cafe", "Vale a les 5! pero, podriem anar al coffebook que hi han donuts :P");
+        AnnaJu128.sendMail(num3);
+        mailStore.getMail("Llop00");
+        Message num4 = new Message("Llop00", "AnnaJu128", "Cafe", "Bueno vale, a les 5 al CoffeBook.");
+        Llop00.sendMail(num4);
+        mailStore.getMail("AnnaJu128");
     }
 }
