e334bbcaffae48220c10ef0eeffc7d91cc3567cf
Anna <annajulia@estudiants.urv.cat>
2020-12-14 10:06:08 +0100

    "Intento main pero soc inutil <3"

--- src/part1/Test.java
+++ src/part1/Test.java
@@ -5,10 +5,17 @@ import java.util.Date;
 // MAIN
 public class Test
 {
-
     public static void main (String[] Args) throws ParseException {
 
         User AnnaJu128 = new User("AnnaJu128", "Anna", User.formatter.parse("18-10-1999"));
-        User Llop99 = new User("Llop99", "David", User.formatter.parse("18-10-1999"));
+        User Llop00 = new User("Llop00", "David", User.formatter.parse("07-11-2000"));
+        MailSystem mailSystem = MailSystem.getMailSystem();
+        mailSystem.setMailStore(OnFileMailStore.getInstance());
+        mailSystem.addUser(AnnaJu128);  mailSystem.addUser(Llop00);
+        Message num1 = new Message("AnnaJu128", "Llop00", "Cafe", "Hola! Vols anar a fer un cafe aquesta tarda??");
+        MailStore mailStore = mailSystem.getMailStore();
+        mailStore.sendMail(num1);
+        mailStore.getMail("Llop00");
+        Message num2 = new Message()
     }
 }
